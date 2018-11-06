85650b769d2f02cd104172ca6578a0f6cfa1e635
MohamedMarzouki <mohamedmarzouki9@gmail.com>
2018-11-06 16:29:08 +0000

    v0.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+  denba: Object;
+  constructor(private http: HttpClient){}
+
+  getDenba () {
+    console.log('denba')
+    this.http.get('http://127.0.0.1:8000/').subscribe((data) => this.denba = data );
+    //waiting for the back response
+    console.log(this.denba);
+  }
 }
